b4f18252d3aff09e8d56c09d2d5eeb458b4d491b
priti <pritibhushanf@gmail.com>
2024-09-14 23:59:13 +0530

    print_help

---
+++ help.py
@@ -0,0 +1,10 @@
+# help(print)
+def test_1(a,b):
+    """
+    :param a:
+    :param b:
+    :return: a+b
+    """
+    return a+b
+
+help(test_1)
---
+++ print.py
@@ -0,0 +1,9 @@
+age=40
+name ="priti"
+
+print("My name is",name , "and i am ",age,"years old")
+#separator
+print("My name is",name , "and i am ",age,"years old",sep=",")
+#end
+print("My name is ",name,end=" |")
+print(" I am ", age , "years old")
