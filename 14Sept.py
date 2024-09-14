e665a5876110ae6881eebba0a95e70e6cf0a0282
priti <pritibhushanf@gmail.com>
2024-09-14 12:38:53 +0530

    14sept

---
+++ 14Sept.py
@@ -0,0 +1,16 @@
+#pythoncalculator :
+
+operator = input("Enter an operator : (+ - * / ):")
+num1 = float(input("Enter the first number : "))
+num2 = float(input("Enter the second number : "))
+
+if operator == "+":
+    result = num1 +num1
+elif operator == "-":
+    result = num1-num2
+elif operator == "*":
+    result = num1*num2
+elif operator == "/":
+    result = num1/num2
+
+print(result)
