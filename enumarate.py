b5da8d64f691bec057d778de781370bc94120275
priti <pritibhushanf@gmail.com>
2024-09-21 08:32:36 +0530

    star_folder_enumrate

---
+++ enumarate.py
@@ -0,0 +1,4 @@
+def run(b,m="",y=20,con=False):
+    return (b,str(y),str(con))
+
+print(run(m = "furr",b="am")[1][1])
--- folder_size.py
+++ folder_size.py
@@ -1,18 +1,26 @@
+# import os
+# import shutil
+#
+# oldpath = "C:\\Users\\Dell\\Downloads"
+# ad = os.listdir(oldpath)
+# for i in ad:
+#     if i.__contains__(".pdf"):
+#         print(i)
+#         MYDIR = "C:\\Users\\Dell\\Downloads\\pdf"
+#         CHECK_FOLDER = os.path.isdir(MYDIR)
+#
+#         if not CHECK_FOLDER:
+#             os.makedirs(MYDIR)
+#             print("created folder : ", MYDIR)
+#
+#         else:
+#             print(MYDIR, "folder already exists.")
+#             shutil.move(oldpath+"\\"+i,MYDIR+"\\"+i)
+
 import os
-import shutil
 
-oldpath = "C:\\Users\\Dell\\Downloads"
-ad = os.listdir(oldpath)
-for i in ad:
-    if i.__contains__(".pdf"):
-        print(i)
-        MYDIR = "C:\\Users\\Dell\\Downloads\\pdf"
-        CHECK_FOLDER = os.path.isdir(MYDIR)
+path_1 = "C:\\13March"
+# for i in os.listdir(path_1):
+#     print(path_1 + "\\"+i)
 
-        if not CHECK_FOLDER:
-            os.makedirs(MYDIR)
-            print("created folder : ", MYDIR)
-
-        else:
-            print(MYDIR, "folder already exists.")
-            shutil.move(oldpath+"\\"+i,MYDIR+"\\"+i)
+print(os.path.getsize(path_1))
